8c9f584cb5b30cd308a929670d19bfd8a5a4aede
dabizidabi <dabizidabi@gmail.com>
2020-06-23 19:48:23 +0300

    add new code

--- Car.py
+++ Car.py
@@ -1,8 +1,8 @@
 class Car():
     """Class create car"""
-    def __init__(self, model, color, price):
+    def __init__(self, mark, model, color, price):
         """initiate car"""
-        self.mark = 'Zaporojec'
+        self.mark = mark
         self.model = model
         self.color = color
         self.price = price
@@ -22,3 +22,22 @@ class Car():
 
     def set_marka(self, new_mark):
         self.mark = new_mark
+
+#Chield class
+class electro_car(Car):
+    """Class to create eletro car"""
+    def __init__(self, mark, model, color, price):
+        """Initiate electo car"""
+        super().__init__(mark, model, color, price)
+        self.__engine_class = 'Electro engine'
+        self.__battery = 100
+
+    def make_battery(self):
+        """Use battery"""
+        self.__battery -= 10
+
+    def show_car(self):
+        dicription = (self.mark + " Model is: " + self.model + " color: " + self.color + ", price is: " + str(self.price) + "$ ," + self.__engine_class + " battery: " + str(self.__battery))
+        print(dicription)
+
+
--- SimpleClass.py
+++ SimpleClass.py
@@ -2,8 +2,8 @@
 from Car import *
 
 
-Mycar1 = Car("T1000", "red", 100000)
-Mycar2 = Car("T500", "blue", 80000)
+Mycar1 = Car("Zaporojec", "T1000", "red", 100000)
+Mycar2 = Car("Zaporojec", "T500", "blue", 80000)
 
 Mycar1.price_up()
 Mycar2.show_car()
@@ -11,3 +11,13 @@ Mycar1.show_car()
 Mycar1.mileage()
 Mycar1.set_marka("Tavria")
 Mycar1.show_car()
+
+
+MyElectroCAr = electro_car("Zaporojec", "v50", "White", 10000)
+
+MyElectroCAr.show_car()
+MyElectroCAr.make_battery()
+MyElectroCAr.make_battery()
+MyElectroCAr.battery = 30   #close to change argument with __variable (encapsul)
+MyElectroCAr.make_battery()
+MyElectroCAr.show_car()
